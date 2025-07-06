1c03cc6dc441e573ad8d0aa0cd57a4ac7f93dd06
Aditya Jain <adityajain77356@gmail.com>
2025-07-06 18:22:58 +0530

    model form is completed for book review

--- book/models.py
+++ book/models.py
@@ -31,5 +31,20 @@ class Book(models.Model):
     genre = models.CharField(max_length=20,choices=GENRE_CHOICES)
     isbn  = models.CharField('ISBN',max_length=13,unique=True)
     publication_date = models.DateField()
-    average_rating = models.DecimalField()
+    average_rating = models.DecimalField(
+        max_digits=3,
+        decimal_places=2,
+        validators=[MinValueValidator(0),MaxValueValidator(5)],
+        default=0
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse("book-detail", kwargs={"pk": self.pk})
+    
+    
     
